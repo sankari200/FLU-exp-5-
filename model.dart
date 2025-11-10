bcf9cd136bf66d762c4bf3226b777b21b63de89f
sankari200 <116174559+sankari200@users.noreply.github.com>
2025-11-10 15:30:08 +0530

    Create model.dart

---
+++ model.dart
@@ -0,0 +1,14 @@
+class Trip {
+  final String name, destination;
+  final int noOfDays, travelCost, hotel, food, other;
+
+  Trip({
+    required this.name,
+    required this.destination,
+    required this.noOfDays,
+    required this.travelCost,
+    required this.hotel,
+    required this.food,
+    required this.other,
+  });
+}
